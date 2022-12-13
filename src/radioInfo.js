8176603fd18db445bc0767969e15f06848598581
deimoss123 <danielsalksnis@gmail.com>
2022-12-13 18:29:22 +0200

    hostēt radio bildes uz ulmanbots.lv

--- src/radioInfo.js
+++ src/radioInfo.js
@@ -1,61 +1,61 @@
 const radioInfo = {
   'Latvijas Radio 1': {
-    img: 'https://i.postimg.cc/j5QsYwK8/lr1.png',
+    img: 'https://www.ulmanbots.lv/images/radio/lr1.png',
     url: 'https://60766ff53d5e6.streamlock.net/liveALR1/mp4:LR1/playlist.m3u8',
     color: '#e00008',
   },
   'Latvijas Radio 2': {
-    img: 'https://i.postimg.cc/mkLTLLgT/lr2.png',
+    img: 'https://www.ulmanbots.lv/images/radio/lr2.png',
     url: 'https://5a44e5b800a41.streamlock.net/shoutcast/mp4:lr2a.stream/playlist.m3u8',
     color: '#ffcc00',
   },
   'Latvijas Radio 3': {
-    img: 'https://i.postimg.cc/vmnYX2Kw/lr3.png',
+    img: 'https://www.ulmanbots.lv/images/radio/lr3.png',
     url: 'https://60766ff53d5e6.streamlock.net/liveALR3/mp4:klasika/playlist.m3u8',
     color: '#000000',
   },
   'Latvijas Radio 4': {
-    img: 'https://i.postimg.cc/sXRVL9FQ/lr4.png',
+    img: 'https://www.ulmanbots.lv/images/radio/lr4.png',
     url: 'https://5a44e5b800a41.streamlock.net/shoutcast/mp4:lr4a.stream/playlist.m3u8',
     color: '#307fe2',
   },
   'Latvijas Radio 5': {
-    img: 'https://i.postimg.cc/jqcs87M2/lr5.png',
+    img: 'https://www.ulmanbots.lv/images/radio/lr5.png',
     url: 'https://live.pieci.lv/live19-hq.mp3',
     color: '#9464fa',
   },
   'Radio SWH': {
-    img: 'https://i.postimg.cc/63DMZ6J0/sw-h.jpg',
+    img: 'https://www.ulmanbots.lv/images/radio/swh.jpg',
     url: 'https://live.radioswh.lv:8443/swhmp3',
     color: '#f67f21',
   },
   'Radio Skonto': {
-    img: 'https://i.postimg.cc/D0LBPV7K/skonto.jpg',
+    img: 'https://www.ulmanbots.lv/images/radio/skonto.jpg',
     url: 'http://stream.radioskonto.lv:8002/stereo',
     color: '#ef3e36',
   },
   'Latgolys Radeja': {
-    img: 'https://i.postimg.cc/sgqfYCwJ/latgales.jpg',
+    img: 'https://www.ulmanbots.lv/images/radio/latgales.png',
     url: 'http://195.13.253.51:8000/128_mp3',
     color: '#155f0a',
   },
   'Latvijas Kristigais Radio': {
-    img: 'https://i.postimg.cc/P5Prv5zx/kristigais.jpg',
+    img: 'https://www.ulmanbots.lv/images/radio/kristigais.jpg',
     url: 'http://91.228.7.124:7007/;?type=http&nocache=905',
     color: '#643c78',
   },
   'Kurzemes Radio': {
-    img: 'https://i.postimg.cc/k4y2X9Hp/kurzemes.png',
+    img: 'https://www.ulmanbots.lv/images/radio/kurzemes.png',
     url: 'http://31.170.16.6:8000/;?type=http&nocache=457',
     color: '#771464',
   },
   'European Hit Radio': {
-    img: 'https://i.postimg.cc/HxYnzhfm/ehr.png',
+    img: 'https://www.ulmanbots.lv/images/radio/ehr.png',
     url: 'https://stream2.superfm.lv:8000/ehr.aac',
     color: '#ed1c24',
   },
   'Star FM': {
-    img: 'https://i.postimg.cc/XJJd2qCQ/starfm.png',
+    img: 'https://www.ulmanbots.lv/images/radio/starfm.png',
     url: 'http://starfm.live.advailo.com/audio/mp3/icecast.audio',
     color: '#ffffff',
   },
