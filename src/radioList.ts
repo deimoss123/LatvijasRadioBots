4581247654053e6bf92337911bd02b25e2f82256
deimoss123 <danielsalksnis@gmail.com>
2024-04-05 20:10:29 +0300

    Izmantot github attēlu linkus

--- src/radioList.ts
+++ src/radioList.ts
@@ -1,28 +1,26 @@
 const radioList = {
   'Latvijas Radio 1': {
-    img: 'https://www.ulmanbots.lv/images/radio/lr1.png',
+    img: 'https://raw.githubusercontent.com/deimoss123/LatvijasRadioBots/dev/assets/full/lr1.png',
     url: 'https://60766ff53d5e6.streamlock.net/liveALR1/mp4:LR1/playlist.m3u8',
-    // url: 'http://lr1mp1.latvijasradio.lv:8012/listen.pls',
-    // url: 'http://lr1mp1.latvijasradio.lv:8012/;stream.mp3',
     color: '#e00008',
   },
   'Latvijas Radio 2': {
-    img: 'https://www.ulmanbots.lv/images/radio/lr2.png',
+    img: 'https://raw.githubusercontent.com/deimoss123/LatvijasRadioBots/dev/assets/full/lr2.png',
     url: 'https://5a44e5b800a41.streamlock.net/shoutcast/mp4:lr2a.stream/playlist.m3u8',
     color: '#ffcc00',
   },
   'Latvijas Radio 3': {
-    img: 'https://www.ulmanbots.lv/images/radio/lr3.png',
+    img: 'https://raw.githubusercontent.com/deimoss123/LatvijasRadioBots/dev/assets/full/lr3.png',
     url: 'https://60766ff53d5e6.streamlock.net/liveALR3/mp4:klasika/playlist.m3u8',
     color: '#000000',
   },
   'Latvijas Radio 4': {
-    img: 'https://www.ulmanbots.lv/images/radio/lr4.png',
+    img: 'https://raw.githubusercontent.com/deimoss123/LatvijasRadioBots/dev/assets/full/lr4.png',
     url: 'https://5a44e5b800a41.streamlock.net/shoutcast/mp4:lr4a.stream/playlist.m3u8',
     color: '#307fe2',
   },
   'Latvijas Radio 5': {
-    img: 'https://www.ulmanbots.lv/images/radio/lr5.png',
+    img: 'https://raw.githubusercontent.com/deimoss123/LatvijasRadioBots/dev/assets/full/lr5.png',
     url: 'https://5a44e5b800a41.streamlock.net/pieci/mp4:k2/chunklist_w379784304.m3u8',
     color: '#9464fa',
   },
@@ -32,27 +30,27 @@ const radioList = {
   //   color: '#ed1c24',
   // },
   'LR Vecās Plates': {
-    img: 'https://live.latvijasradio.lv/_next/image?url=https%3A%2F%2Fcdn.pieci.lv%2Fmedia%2Fbranding%2F640%2Fvecas-plates_l.jpg&w=640&q=75',
+    img: 'https://raw.githubusercontent.com/deimoss123/LatvijasRadioBots/dev/assets/full/lr-vecas-plates.jpg',
     url: 'https://5a44e5b800a41.streamlock.net/pieci/mp4:k4/chunklist_w1071824452.m3u8',
     color: '#dfcead',
   },
   'Radio SWH': {
-    img: 'https://www.ulmanbots.lv/images/radio/swh.jpg',
+    img: 'https://raw.githubusercontent.com/deimoss123/LatvijasRadioBots/dev/assets/full/swh.jpg',
     url: 'https://live.radioswh.lv:8443/swhmp3',
     color: '#f67f21',
   },
   'Radio SWH Rock': {
-    img: 'https://i.postimg.cc/nzQX2Jyc/sw-h-rock.png',
+    img: 'https://raw.githubusercontent.com/deimoss123/LatvijasRadioBots/dev/assets/full/swh-rock.png',
     url: 'https://live.radioswh.lv:8443/rockmp3',
     color: '#c93440',
   },
   'Radio Skonto': {
-    img: 'https://www.ulmanbots.lv/images/radio/skonto.jpg',
+    img: 'https://raw.githubusercontent.com/deimoss123/LatvijasRadioBots/dev/assets/full/skonto.jpg',
     url: 'http://stream.radioskonto.lv:8002/stereo',
     color: '#ef3e36',
   },
   'Latgolys Radeja': {
-    img: 'https://www.ulmanbots.lv/images/radio/latgales.png',
+    img: 'https://raw.githubusercontent.com/deimoss123/LatvijasRadioBots/dev/assets/full/latgales.png',
     url: 'http://195.13.253.51:8000/128_mp3',
     color: '#155f0a',
   },
@@ -62,17 +60,17 @@ const radioList = {
   //   color: '#643c78',
   // },
   'Kurzemes Radio': {
-    img: 'https://www.ulmanbots.lv/images/radio/kurzemes.png',
+    img: 'https://raw.githubusercontent.com/deimoss123/LatvijasRadioBots/dev/assets/full/kurzemes.png',
     url: 'http://31.170.16.6:8000/;?type=http&nocache=457',
     color: '#771464',
   },
   'European Hit Radio': {
-    img: 'https://www.ulmanbots.lv/images/radio/ehr.png',
+    img: 'https://raw.githubusercontent.com/deimoss123/LatvijasRadioBots/dev/assets/full/ehr.png',
     url: 'https://stream2.superfm.lv:8000/ehr.aac',
     color: '#ed1c24',
   },
   'Star FM': {
-    img: 'https://www.ulmanbots.lv/images/radio/starfm.png',
+    img: 'https://raw.githubusercontent.com/deimoss123/LatvijasRadioBots/dev/assets/full/starfm.png',
     url: 'http://starfm.live.advailo.com/audio/mp3/icecast.audio',
     color: '#ffffff',
   },
